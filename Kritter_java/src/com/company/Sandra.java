d10a5a365ba158712e80eed95d7e556a0ff88592
Sandra602 <sandramariya602@gmail.com>
2021-12-09 16:32:21 +0530

    Second Commit

--- Kritter_java/src/com/company/Sandra.java
+++ Kritter_java/src/com/company/Sandra.java
@@ -1,7 +1,18 @@
 package com.company;
 
+import org.omg.Messaging.SyncScopeHelper;
+
 public class Sandra {
     public static void main(String[] args) {
-        System.out.print("Sandra");
+        System.out.println("Sandra");
+        int num1= 45;
+        int num2= 59;
+            if (num1>num2) {
+                System.out.println("Number1 is greater than NUmber2");
+            }
+            else {
+                System.out.println("NUmber2 is greater");
+
+        }
     }
 }
