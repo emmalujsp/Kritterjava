d0459ec2c144f4429ba4940297fdd46c37e86b43
Masss12345 <sureshadarsh516@gmail.com>
2021-12-09 16:08:41 +0530

    chumma tried

--- Kritter_java/src/com/company/Adarsh.java
+++ Kritter_java/src/com/company/Adarsh.java
@@ -4,5 +4,8 @@ public class Adarsh {
     public static void main(String[] args) {
         System.out.println("emmalu");
         System.out.print("Adarsh");
+        System.out.print("Sample program");
+        System.out.print("Trail");
+
     }
 }
