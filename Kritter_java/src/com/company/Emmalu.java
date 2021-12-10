7c3fd638997706547079ce7557c829bab56ee31a
emmalujsp <emmalujsp@gmail.com>
2021-12-10 12:31:16 +0530

    New things added

--- Kritter_java/src/com/company/Emmalu.java
+++ Kritter_java/src/com/company/Emmalu.java
@@ -5,20 +5,51 @@ class SBI
     //changes by adarsh
     Scanner sc = new Scanner(System.in);
     int id;
- String name;
- float balance;
- char gender;
- void add()
- {
-     System.out.print("Enter name :");
-     String name = sc.nextLine();
- }
+    String name;
+    float balance;
+    char gender;
+    void add()
+    {
+        System.out.print("Enter name :");
+        name = sc.nextLine();
+        System.out.print("Enter id :");
+        id = sc.nextInt();
+        System.out.print("Enter Balance :");
+        balance = sc.nextFloat();
+        System.out.print("Enter gender(m/f) :");
+        gender = sc.next().charAt(0);
+    }
+    void print()
+    {
+        System.out.println("Name :"+name);
+        System.out.println("Id :"+id);
+        System.out.println("Balance :"+balance);
+        System.out.println("Gender :"+gender);
+    }
 
 
 }
 public class Emmalu {
-    public static void main(String[] args)
-    {
-        System.out.print("emmalu");
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        SBI client1 = new SBI();
+        char ch;
+        System.out.println("Welcome");
+        do {
+            {
+                System.out.print("1. Add Details\n2. Print Details\nEnter your Choice :");
+                int choice = sc.nextInt();
+                switch (choice) {
+                    case 1:
+                        client1.add();
+                        break;
+                    case 2:
+                        client1.print();
+                        break;
+                }
+            }
+            System.out.print("Do you want to continue(y/n)?  :");
+            ch = sc.next().charAt(0);
+        } while (ch == 'y' | ch == 'Y');
     }
 }
