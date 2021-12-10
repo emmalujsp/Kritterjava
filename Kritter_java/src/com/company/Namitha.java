0c4cc3e2efdfc27555278602f3b1e365fd341dc2
Namitha1999 <namithaantony19@gmail.com>
2021-12-10 12:09:52 +0530

    customer details

--- Kritter_java/src/com/company/Namitha.java
+++ Kritter_java/src/com/company/Namitha.java
@@ -1,7 +1,39 @@
 package com.company;
+import java.util.*;
 
+class Customer
+{
+    Scanner sc = new Scanner(System.in);
+    String name;
+    int age;
+    String gender;
+    String place;
+
+    void details()
+    {
+        System.out.print("Enter the Name: ");
+        name=sc.nextLine();
+        System.out.print("Enter the age: ");
+        age=sc.nextInt();
+        gender=sc.nextLine();
+        System.out.print("Enter the Gender: ");
+        gender=sc.nextLine();
+        System.out.print("Enter the place: ");
+        place = sc.nextLine();
+    }
+
+    void display()
+    {
+        System.out.println("Welcome, " +name+"!");
+        System.out.println("Age: "+age);
+        System.out.println("Gender: "+gender);
+        System.out.println("City: "+place);
+    }
+}
 public class Namitha {
     public static void main(String[] args) {
-        System.out.print("emmalu");
+        Customer cust = new Customer();
+        cust.details();
+        cust.display();
     }
 }
